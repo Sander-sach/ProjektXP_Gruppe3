b4f665525aba8c526c08f60c4fdea14c6ee70eeb
Hother <Soren.hother@gmail.com>
2026-09-25 11:04:56 +0200

    Changed movierepo to interface created movieService

--- src/main/java/com/example/bioproject/repositories/MovieRepo.java
+++ src/main/java/com/example/bioproject/repositories/MovieRepo.java
@@ -1,4 +1,11 @@
 package com.example.bioproject.repositories;
 
-public class MovieRepo {
+import com.example.bioproject.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MovieRepo extends JpaRepository<Movie,Long> {
+
 }
---
+++ src/main/java/com/example/bioproject/services/MovieService.java
@@ -0,0 +1,5 @@
+package com.example.bioproject.services;
+
+public class MovieService {
+
+}
