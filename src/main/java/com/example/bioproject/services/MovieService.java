d24e10da564ab5cb8ddd7d56d3c197b33b5a5609
Hother <Soren.hother@gmail.com>
2026-09-25 12:11:15 +0200

    Movieservice findall should work

--- src/main/java/com/example/bioproject/services/MovieService.java
+++ src/main/java/com/example/bioproject/services/MovieService.java
@@ -1,5 +1,23 @@
 package com.example.bioproject.services;
 
+import com.example.bioproject.entities.Movie;
+import com.example.bioproject.repositories.MovieRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+
+    private final MovieRepo movieRepo;
+
+    public MovieService(MovieRepo moveRepo) {
+        this.movieRepo = moveRepo;
+    }
+
+    public List<Movie> findAllMovies(){
+        return movieRepo.findAll();
+    }
+
 
 }
